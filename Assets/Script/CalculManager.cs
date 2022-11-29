cf037c9e51d10a5480239f83d4a50d657ab842b2
SbyreXII <josselin.renan@gmail.com>
2022-11-29 15:26:30 +0100

    Update Calcul Manager

--- Assets/Script/CalculManager.cs
+++ Assets/Script/CalculManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class CalculManager : MonoBehaviour
 {
+    [Header("Listes")]
+    [SerializeField] private List<List<int>> m_calculList;
+    [SerializeField] private List<int> m_answerList;
+    private List<int> tupleNumber;
+    private List<int> m_fauteList;
+
+    [Header("Choice")]
+    [SerializeField] private int m_choiceUser;
+
+    [Header("Life")]
+    [SerializeField] GameObject m_lifeCount;
+
+    [Header("Other Variable")]
+    [SerializeField] private int m_questionNumber = 0;
+    private int m_point = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,22 +37,41 @@ public class CalculManager : MonoBehaviour
 
     }
 
-    void LevelOne()
+    void LevelOneRandom()
     {
         int firstNumber = Random.Range(0,10);
         int secondNumber = Random.Range(0, 10);
+
+        tupleNumber.Add(firstNumber);
+        tupleNumber.Add(secondNumber);
+
+        m_calculList.Add(tupleNumber);
+        tupleNumber.Clear();
 
         int sign = Random.Range(0, 1);
 
         if(sign == 0)
         {
             int answer = firstNumber + secondNumber;
-
+            m_answerList.Add(answer);
         }
         else if(sign == 1)
         {
             int answer = firstNumber - secondNumber;
+            m_answerList.Add(answer);
+        }
+    }
 
+
+    void CheckAnswer()
+    {
+        if(m_choiceUser == m_answerList[m_questionNumber])
+        {
+            m_point += 1;
+        }
+        else if(m_choiceUser != m_answerList[m_questionNumber])
+        {
+            m_fauteList.Add(m_questionNumber);
         }
     }
 }
